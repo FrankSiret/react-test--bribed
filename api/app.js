972d3ae80ec655fb24bb311ad37ad47428ebeb02
danielm-qva <frank.siret@gmail.com>
2021-11-07 22:22:52 -0500

    adding swagger to api

--- api/app.js
+++ api/app.js
@@ -4,16 +4,21 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const cors = require('cors');
-const config = require('./config');
+const config = require('config');
 const mongoose = require('mongoose');
+
+const swaggerUi = require('swagger-ui-express');
 
 var apiRouter = require('./routes/api');
 
 var app = express();
 
+const MONGODB_URI = config.get('db');
+const PORT = config.get('app.port');
+
 // connect to mongo
 mongoose
-  .connect(config.MONGODB_URI, {})
+  .connect(MONGODB_URI, {})
   .then(() => console.log('mongodb connected...'))
   .catch(err => console.log(err));
 
@@ -22,9 +27,11 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-// app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', apiRouter);
+const swaggerDocument = require('./swagger.json');
+
+app.use('/v1/bribed', apiRouter);
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
@@ -42,6 +49,6 @@ app.use(function (err, req, res, next) {
   res.render('error');
 });
 
-app.listen(config.PORT, () => console.log(`server started on port ${config.PORT}`));
+app.listen(PORT, () => console.log(`server started on port ${PORT}`));
 
 module.exports = app;
